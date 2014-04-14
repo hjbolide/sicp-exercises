d43d2dd09be6ba498ddb1590d1366980606bce12
hjbolide <hjbolide@gmail.com>
2014-04-15 00:00:58 +1000

    chapter1.2

--- exercise1.scm
+++ exercise1.scm
@@ -67,8 +67,29 @@
 (define (cube-root x)
   (cube-iter 1.0 x))
 
+;; sample nesting
+(define (sqrt x)
+  (define (good-enough? guess x)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess x)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess x)
+    (if (good-enough? guess x)
+        guess
+        (sqrt-iter (improve guess x) x)))
+  (sqrt-iter 1.0 x))
 
-
+;; sample nesting 2
+(define (sqrt x)
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+        guess
+        (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
 
 
 
